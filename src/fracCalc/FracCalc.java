f1a33cbc97b5d1c9840cf53ccca57fa61e690935
majorjoel2 <joel@tanner-family.org>
2015-11-29 13:57:07 -0500

    added part 2 not tested and added comments

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -25,7 +25,8 @@ public class FracCalc {
         // TODO: Implement this function to produce the solution to the input
     	String part1 = "";
     	String part2 = "";
-    	int function; //0 = +; 1 = -; 2 = *; 3 = /;
+    	int function = 4; //0 = +; 1 = -; 2 = *; 3 = /;
+    	int returntype = 1;
         String num1 = "";
         int num1i;
         String top1 = "";
@@ -39,7 +40,15 @@ public class FracCalc {
         String bottom2 = "";
         int bottom2i;
         
-        if(input.contains("+")) {
+        String output = "";
+        
+        //split into parts
+        String[] response = input.split(" ");
+        part1 = response[0];
+        part2 = response[2];
+        
+        //remove because it is clunky and stupid
+        /*if(input.contains("+")) {
     	    //addition
     	    part1 = input.substring(0, (input.indexOf("+") - 1));
     	    part2 = input.substring(input.indexOf("+") + 2);
@@ -62,21 +71,26 @@ public class FracCalc {
  			       }
  		       }
  	       }
+        }*/
+        
+        if(returntype == 1) {
+        	return part2;
         }
         
-        /*if(part1.contains("_")) {
+        //split into each number
+        if(part1.contains("_")) {
         	num1 = part1.substring(0, part1.indexOf("_"));
         	top1 = part1.substring(part1.indexOf("_") + 1, part1.indexOf("/"));
         	bottom1 = part1.substring(part1.indexOf("/") + 1);
         } else {
         	if(part1.contains("/")) {
-	        	num1 = "";
+	        	num1 = "0";
 	        	top1 = part1.substring(0, part1.indexOf("/"));
 	        	bottom1 = part1.substring(part1.indexOf("/") +1);
         	} else {
         		num1 = part1;
-        		top1 = "";
-        		bottom1 = "";
+        		top1 = "0";
+        		bottom1 = "0";
         	}
         }
         if(part2.contains("_")) {
@@ -85,21 +99,49 @@ public class FracCalc {
         	bottom2 = part2.substring(part2.indexOf("/") + 1);
         } else {
         	if(part2.contains("/")) {
-	        	num2 = "";
+	        	num2 = "0";
 	        	top2 = part2.substring(0, part2.indexOf("/"));
 	        	bottom2 = part2.substring(part2.indexOf("/") +1);
         	} else {
         		num2 = part2;
-        		top2 = "";
-        		bottom2 = "";
+        		top2 = "0";
+        		bottom2 = "0";
         	}
         }
-        /*if(num2.equals("")) {
-        	return top2 + "/" + bottom2;
-        } else {
-        	return num2 + "_" + top2 + "/" + bottom2;
-        }*/
-        return part2;
+        
+        if(returntype == 2) {
+        	return "whole:" + num2 + " numerator:" + top2 + " denominator:" + bottom2;
+        }
+        
+        //get function type
+        if(response[1].equals("+")) {
+        	function = 0;
+        }
+        if(response[1].equals("-")) {
+        	function = 1;
+        }
+        if(response[1].equals("*")) {
+        	function = 2;
+        }
+        if(response[1].equals("-")) {
+        	function = 3;
+        }
+        if(function == 4) {
+        	//fail safe
+        	return "FAILED";
+        }
+        
+        //convert string to integer
+        num1i = Integer.parseInt(num1);
+        top1i = Integer.parseInt(top1);
+        bottom1i = Integer.parseInt(bottom1);
+        num2i = Integer.parseInt(num2);
+        top2i = Integer.parseInt(top2);
+        bottom2i = Integer.parseInt(bottom2);
+        
+        //end of function reached fail safe
+        return "FAILED";
+        
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
