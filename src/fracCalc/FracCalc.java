b5305e2c8efcb5b1a213be38d83439151a935d1d
majorjoel2 <joel@tanner-family.org>
2015-12-03 09:38:46 -0500

    stuff

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -12,21 +12,39 @@ public class FracCalc {
     	System.out.println(produceAnswer(userInput));
     }
     
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+    static String num = "";
+    static String top = "";
+    static String bottom = "";
+    public static String[] splitPart(String part) {
+    	String[] output = new String[3];
+        if(part.contains("_")) {
+        	num = part.substring(0, part.indexOf("_"));
+        	top = part.substring(part.indexOf("_") + 1, part.indexOf("/"));
+        	bottom = part.substring(part.indexOf("/") + 1);
+        } else {
+        	if(part.contains("/")) {
+	        	num = "0";
+	        	top = part.substring(0, part.indexOf("/"));
+	        	bottom = part.substring(part.indexOf("/") +1);
+        	} else {
+        		num = part;
+        		top= "0";
+        		bottom = "1";
+        	}
+        }
+        output[0] = num;
+        output[1] = top;
+        output[2] = bottom;
+        return output;
+    }
+    
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
     	String part1 = "";
     	String part2 = "";
     	int function = 4; //0 = +; 1 = -; 2 = *; 3 = /;
-    	int returntype = 2;
+    	int returntype = 3;
         String num1 = "";
         int num1i;
         String top1 = "";
@@ -39,6 +57,11 @@ public class FracCalc {
         int top2i;
         String bottom2 = "";
         int bottom2i;
+        int numOut;
+        int topOut;
+        int bottomOut;
+        boolean is1Neg;
+        boolean is2Neg;
         
         String output = "";
         
@@ -46,68 +69,20 @@ public class FracCalc {
         String[] response = input.split(" ");
         part1 = response[0];
         part2 = response[2];
-        
-        //remove because it is clunky and stupid
-        /*if(input.contains("+")) {
-    	    //addition
-    	    part1 = input.substring(0, (input.indexOf("+") - 1));
-    	    part2 = input.substring(input.indexOf("+") + 2);
-    	    function = 0;
-        } else {
- 	       if(input.contains("-")) {
- 	    	   //subtraction
- 	    	   part1 = input.substring(0, (input.lastIndexOf("-") - 1));
- 	    	   part2 = input.substring(input.lastIndexOf("-") + 2);
- 	    	   function = 1;
- 	       } else {
- 		       if(input.contains("*")) {
- 		    	   //multiplication
- 		    	   part1 = input.substring(0, (input.indexOf("*") - 1));
- 		    	   part2 = input.substring(input.indexOf("*") + 2);
- 		    	   function = 2;
- 		       } else {
- 			       if(input.contains("/")) {
- 			    	   //division
- 			       }
- 		       }
- 	       }
-        }*/
-        
+                
         if(returntype == 1) {
         	return part2;
         }
         
         //split into each number
-        if(part1.contains("_")) {
-        	num1 = part1.substring(0, part1.indexOf("_"));
-        	top1 = part1.substring(part1.indexOf("_") + 1, part1.indexOf("/"));
-        	bottom1 = part1.substring(part1.indexOf("/") + 1);
-        } else {
-        	if(part1.contains("/")) {
-	        	num1 = "0";
-	        	top1 = part1.substring(0, part1.indexOf("/"));
-	        	bottom1 = part1.substring(part1.indexOf("/") +1);
-        	} else {
-        		num1 = part1;
-        		top1 = "0";
-        		bottom1 = "1";
-        	}
-        }
-        if(part2.contains("_")) {
-        	num2 = part2.substring(0, part2.indexOf("_"));
-        	top2 = part2.substring(part2.indexOf("_") + 1, part2.indexOf("/"));
-        	bottom2 = part2.substring(part2.indexOf("/") + 1);
-        } else {
-        	if(part2.contains("/")) {
-	        	num2 = "0";
-	        	top2 = part2.substring(0, part2.indexOf("/"));
-	        	bottom2 = part2.substring(part2.indexOf("/") +1);
-        	} else {
-        		num2 = part2;
-        		top2 = "0";
-        		bottom2 = "1";
-        	}
-        }
+        String[] partA = splitPart(part1);
+        String[] partB = splitPart(part2);
+        num1 = partA[0];
+        top1 = partA[1];
+        bottom1 = partA[2];
+        num2 = partB[0];
+        top2 = partB[1];
+        bottom2 = partB[2];
         
         if(returntype == 2) {
         	return "whole:" + num2 + " numerator:" + top2 + " denominator:" + bottom2;
@@ -130,6 +105,16 @@ public class FracCalc {
         	//fail safe
         	return "FAILED";
         }
+        if(num1.contains("-") || top1.contains("-")) {
+        	is1Neg = true;
+        } else {
+        	is1Neg = false;
+        }
+        if(num2.contains("-") || top2.contains("-")) {
+        	is2Neg = true;
+        } else {
+        	is2Neg = false;
+        }
         
         //convert string to integer
         num1i = Integer.parseInt(num1);
@@ -138,6 +123,31 @@ public class FracCalc {
         num2i = Integer.parseInt(num2);
         top2i = Integer.parseInt(top2);
         bottom2i = Integer.parseInt(bottom2);
+        
+        System.out.println(num1i);
+        System.out.println(top1i);
+        System.out.println(bottom1i);
+        System.out.println(is1Neg);
+        System.out.println(num2i);
+        System.out.println(top2i);
+        System.out.println(bottom2i);
+        System.out.println(is2Neg);
+        
+        numOut = 0;
+        topOut = 0;
+        bottomOut = 0;
+        if(function == 0) {
+        	//addition
+        	if(num1i != 0 && num2i != 0) {
+        		numOut = num1i + num2i;
+        	}
+        	if(bottom1i == bottom2i && is1Neg == false && is2Neg == false) {
+        		topOut = top1i + top2i;
+        		bottomOut = bottom1i;
+        	}
+        }
+        
+        System.out.println(numOut + "_" + topOut + "/" + bottomOut);
         
         //end of function reached fail safe
         return "FAILED";
