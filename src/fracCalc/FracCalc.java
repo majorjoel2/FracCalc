d0e5998c6200209b2118ab4470dba42e119b2935
Joel Tanner <joel@tanner-family.org>
2015-12-07 22:13:07 -0500

    passes all but the more than two fraction inputs

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -38,6 +38,22 @@ public class FracCalc {
         return output;
     }
     
+    public static int GCF(int num, int den) {
+    	while (den > 0)
+        {
+            int temp = den;
+            den = num % den;
+            num = temp;
+        }
+        return num;
+    }
+    
+    public static int[] reduceFraction(int num, int den){
+        int gcf = GCF(num, den); //greatest common factor
+        int[] rf = {num/gcf, den/gcf};
+        return rf;
+    }
+    
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
@@ -98,7 +114,7 @@ public class FracCalc {
         if(response[1].equals("*")) {
         	function = 2;
         }
-        if(response[1].equals("-")) {
+        if(response[1].equals("/")) {
         	function = 3;
         }
         if(function == 4) {
@@ -124,21 +140,21 @@ public class FracCalc {
         top2i = Integer.parseInt(top2);
         bottom2i = Integer.parseInt(bottom2);
         
-        System.out.println(num1i);
+/*        System.out.println(num1i);
         System.out.println(top1i);
         System.out.println(bottom1i);
         System.out.println(is1Neg);
         System.out.println(num2i);
         System.out.println(top2i);
         System.out.println(bottom2i);
-        System.out.println(is2Neg);
+        System.out.println(is2Neg);*/
         
         numOut = 0;
         topOut = 0;
         bottomOut = 0;
         if(function == 0) {
         	//addition
-        	if((top1i != 0 && top2i != 0) || (top1i == 0 && top2i == 0)) {
+        	if((top1i != 0 && top2i != 0) || (top1i == 0 && top2i == 0) || num1i == 0 || num2i == 0) {
         		numOut = num1i + num2i;
         	} else {
         		numOut = 0;
@@ -188,29 +204,196 @@ public class FracCalc {
         		topOut = top1i + top2i;
         	}
         	
-        	while(topOut < -bottomOut) {
+        	while(topOut <= -bottomOut) {
         		topOut += bottomOut;
         		numOut--;
         	}
         	if(topOut < 0) {
         		topOut -= topOut*2;
         	}
-        	while(topOut > bottomOut) {
+        	while(topOut >= bottomOut) {
         		topOut -= bottomOut;
         		numOut++;
         	}
         }
         
-        if(returntype == 3) {
-        	if(numOut == 0) {
-        		return topOut + "/" + bottomOut;
+        if(function == 1) {
+        	//subtraction
+        	if((top1i != 0 && top2i != 0) || (top1i == 0 && top2i == 0) || num1i == 0 || num2i == 0) {
+        		numOut = num1i - num2i;
+        	} else {
+        		numOut = 0;
+        		if(is1Neg == false) {
+        			top1i = num1i*bottom1i - top1i;
+        		} else {
+        			top1i += num1i*bottom1i;
+        		}
+        		if(is1Neg == false) {
+        			top2i = num2i*bottom2i - top2i;
+        		} else {
+        			top2i += num2i*bottom2i;
+        		}
+        	}
+        	boolean omit1 = false;
+        	if(bottom1i == 1 && bottom2i != 1) {
+        		top1i = top1i*bottom2i;
+        		bottom1i = bottom2i;
+        		omit1 = true;
+        	} else {
+	        	if(bottom1i != 1 && bottom2i == 1) {
+	        		top2i = top2i*bottom1i;
+	        		bottom2i = bottom1i;
+	        		omit1 = true;
+	        	}
+        	}
+        	if(bottom1i == bottom2i) {
+        		if(omit1 == false && is1Neg == true && (top1.contains("-") == false)) {
+        			top1i -= top1i*2;
+        		}
+        		if(omit1 == false && is2Neg == true && (top2.contains("-") == false)) {
+        			top2i -= top2i*2;
+        		}
+        		topOut = top1i - top2i;
+        		bottomOut = bottom1i;
+        	}
+        	if(bottom1i != bottom2i) {
+        		top1i = top1i*bottom2i;
+        		top2i = top2i*bottom1i;
+        		bottomOut = bottom1i*bottom2i;
+        		if(omit1 == false && is1Neg == true && (top1.contains("-") == false)) {
+        			top1i -= top1i*2;
+        		}
+        		if(omit1 == false && is2Neg == true && (top2.contains("-") == false)) {
+        			top2i -= top2i*2;
+        		}
+        		topOut = top1i - top2i;
+        	}
+        	
+        	if(topOut > 0 && numOut < 0) {
+        		numOut++;
+        		topOut = Math.abs(topOut);
+        		topOut -= bottomOut;
+        	}
+        	while(topOut <= -bottomOut) {
+        		topOut += bottomOut;
+        		numOut--;
+        	}
+        	if(topOut < 0 && numOut != 0) {
+        		topOut = Math.abs(topOut);
+        	}
+        	while(topOut >= bottomOut) {
+        		topOut -= bottomOut;
+        		numOut++;
         	}
         }
-        System.out.println(numOut + "_" + topOut + "/" + bottomOut);
+        
+        if(function == 2) {
+        	//multiplication
+        	if(is1Neg == false) {
+        		top1i = num1i * bottom1i + top1i;
+        	}
+        	if(is1Neg == true) {
+        		top1i = num1i * bottom1i - Math.abs(top1i);
+        	}
+        	if(is2Neg == false) {
+        		top2i = num2i * bottom2i + top2i;
+        	}
+        	if(is2Neg == true) {
+        		top2i = num2i * bottom2i - Math.abs(top2i);
+        	}
+        	numOut = 0;
+        	topOut = top1i * top2i;
+        	bottomOut = bottom1i * bottom2i;
+        	
+        	while(topOut <= -bottomOut) {
+        		topOut += bottomOut;
+        		numOut--;
+        	}
+        	if(topOut < 0) {
+        		topOut -= topOut*2;
+        	}
+        	while(topOut >= bottomOut) {
+        		topOut -= bottomOut;
+        		numOut++;
+        	}
+        }
+        
+        if(function == 3) {
+        	//division
+        	if(is1Neg == false) {
+        		top1i = num1i * bottom1i + top1i;
+        	}
+        	if(is1Neg == true) {
+        		top1i = num1i * bottom1i - Math.abs(top1i);
+        	}
+        	if(is2Neg == false) {
+        		top2i = num2i * bottom2i + top2i;
+        	}
+        	if(is2Neg == true) {
+        		top2i = num2i * bottom2i - Math.abs(top2i);
+        	}
+        	numOut = 0;
+        	topOut = top1i * bottom2i;
+        	bottomOut = bottom1i * top2i;
+    
+        	if(Math.abs(topOut) == 0 || Math.abs(bottomOut) == 0) {
+        		numOut = 0;
+        		topOut = 0;
+        		bottomOut = 1;
+        	}
+        	
+        	if(topOut < 0 && bottomOut < 0) {
+        		topOut = Math.abs(topOut);
+        		bottomOut = Math.abs(bottomOut);
+        	}
+        	if(bottomOut < 0 && topOut > 0) {
+        		topOut -= topOut * 2;
+        		bottomOut = Math.abs(bottomOut);
+        	}
+        	
+        	while(topOut <= -bottomOut) {
+        		topOut += bottomOut;
+        		numOut--;
+        	}
+        	if(topOut < 0 && numOut != 0) {
+        		topOut = Math.abs(topOut);
+        	}
+        	while(topOut >= bottomOut) {
+        		topOut -= bottomOut;
+        		numOut++;
+        	}
+        }
+        
+        //reduce
+        boolean isNeg = false;
+        if(topOut < 0) {
+        	isNeg = true;
+        }
+        int[] fraction = new int[2];
+        fraction = reduceFraction(Math.abs(topOut), bottomOut);
+        if(isNeg == false) {
+        	topOut = fraction[0];
+        } else {
+        	topOut = -fraction[0];
+        }
+        bottomOut = fraction[1];
+        
+        if(returntype == 3) {
+        	if(numOut == 0 && topOut != 0) {
+        		return topOut + "/" + bottomOut;
+        	}
+        	if(numOut != 0) {
+        		if(topOut == 0) {
+        			return "" + numOut;
+        		} else {
+        			return numOut + "_" + topOut + "/" + bottomOut;
+        		}
+        	}
+        	return "" + numOut;
+        }
         
         //end of function reached fail safe
         return "FAILED";
-        
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
